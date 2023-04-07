0d1540e4c982a306a2dab3c383e62e40068e9da0
David <david.dev@outlook.com>
2023-04-07 09:25:42 +0000

    script to install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,6 @@
+pip install tensorflow==2.10.*
+pip install tensorflow-datasets==4.8.*
+pip install mediapipe==0.9.*
+pip install pandas==2.0.*
+sudo apt-get update
+sudo apt-get install ffmpeg libsm6 libxext6  -y
